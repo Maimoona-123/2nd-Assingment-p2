17f37913593ad22c86599c9a32366f447c6b1544
maimoona <maimoonaaltaf7@gmail.com>
2024-09-10 01:31:03 -0700

    Assingment p-2

---
+++ app.js
@@ -0,0 +1,54 @@
+// Chap-8      ( (Concatenating Text strings )
+
+// Q-1
+// var num = "2" + "2 ";
+// alert ( num );
+
+
+// Q-2
+// var msg = ( "Hello," + "Dolly");
+// alert ( msg );
+
+
+// Q-3
+// alert ( "33" + 3 );
+
+
+// Q-4;
+// alert ( "Pakistan Zindabad" );
+
+
+// Q-5
+// var num = 3 + 4;
+// alert ( num );
+
+
+
+
+// Chap-9     ( Prompts )
+
+
+
+// Q-1
+// prompt ( "Enter first name" );
+
+
+
+// Q-2
+// prompt ( "Country" );
+
+
+
+// Q-3
+var name = "Your Name";
+prompt ( "Enter your Name" );
+
+
+
+// Q-4
+prompt ( "Enter age" );
+
+
+
+// Q-6
+prompt ( "Welcome to my JS page & Enter your Email" );
